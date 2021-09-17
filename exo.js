4e7c79ece617a5edd80f71f7b856d369bf228365
lotusbleudesign <lotus.b78@gmail.com>
2021-09-17 17:12:40 +0200

    Version 2

--- exo.js
+++ exo.js
@@ -1,27 +1,27 @@
-const { Client } = require("pg");
+const { Client } = require("pg")
 const client = new Client({
-  connectionString: "postgresql://localhost/pg-lesson-one",
+  connectionString: "postgresql://localhost/pg-lesson-one"
 });
 
-client.connect();
+client.connect()
 
-//afficher me nom en fonction de l'id
+//afficher nom en fonction de l'id
 async function getStudents(id){
-    const result = await client.query(`SELECT (name) from students where id= ${id}`);
-    console.log(result.rows);
+    const result = await client.query(`SELECT (name) from students where id= ${id}`)
+    console.log(`SELECT (name) from students where id= ${id} : ${result.rows}`)
 }
 
 // Mettre à jour
 async function updateStudent(name,id) {
-    const result = await client.query("UPDATE students SET name=($1) where (id)=($2)", [name,id]);
-    console.log(`User updated for ID ${id} name ${name}`);
+    const result = await client.query("UPDATE students SET name=($1) where (id)=($2)", [name,id])
+    console.log(`UPDATE students SET name=($1) where (id)=($2)", [name,id] : User updated for ID ${id} name ${name}`)
 }
 
 // delete
 async function deleteStudent (id) {
     const results = await client.query(
-      "DELETE FROM students WHERE id=($1)",[id]);
-    console.log(`Delete succed for ID ${id}`);
+      "DELETE FROM students WHERE id=($1)",[id])
+    console.log(`DELETE FROM students WHERE id=($1)",[id] : Delete succed for ID ${id}`)
   }
 
 // inserer
@@ -30,18 +30,17 @@ async function addStudent(name){
           return;
       }
       const result = await client.query("INSERT INTO students (name) VALUES ($1) RETURNING *", [name]);
-      console.log(`Job done for ${name} ${result.rows[0]}!` ); // we are using [0] because there is only 1 record here.s
+      console.log(`INSERT INTO students (name) VALUES ($1) : Job done for ${name} ${result.rows[0]}!` ); // we are using [0] because there is only 1 record here.s
     }
-
 
 // afficher
 async function getStudents() {
   const result = await client.query("SELECT * from students");
-  console.log(result.rows);
+  console.log(`SELECT * from students ${result.rows}`)
 }
 
-addStudent();
+addStudent()
 deleteStudent(14)
-updateStudent('toto',2);
-getStudents(12).then(() => process.exit(0));
-getStudents().then(() => process.exit(0));
+updateStudent('toto',2)
+getStudents(12)
+getStudents().then(() => process.exit(0))
